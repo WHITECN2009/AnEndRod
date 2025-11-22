ac6c1425dfea161d97e989f9687e1b4e9fc698d5
N501YHappy <n501yhappy@outlook.com>
2025-11-22 23:53:06 +0800

    Hellow!

--- src/main/java/org/WHITECN/anendrod.java
+++ src/main/java/org/WHITECN/anendrod.java
@@ -2,6 +2,13 @@ package org.WHITECN;
 
 import org.WHITECN.commands.rodMerge;
 import org.WHITECN.rods.RegularRod;
+import org.WHITECN.rods.SlimeRod;
+import org.WHITECN.utils.rodItemGenerator;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
@@ -16,8 +23,10 @@ public final class anendrod extends JavaPlugin {
         instance = this;
         logger = getLogger();
         logger.info("插件已启用喵");
+        getServer().getPluginManager().registerEvents(new SlimeRod(),this);
         getServer().getPluginManager().registerEvents(new RegularRod(),this);
         Objects.requireNonNull(this.getCommand("rodmerge")).setExecutor(new rodMerge(this));
+        registerSlimeRodRecipes();
     }
 
     @Override
@@ -27,5 +36,15 @@ public final class anendrod extends JavaPlugin {
 
     public static anendrod getInstance() {
         return instance;
+    }
+    private void registerSlimeRodRecipes() {
+        ItemStack SlimeRod = rodItemGenerator.createSlimeRod();
+        NamespacedKey key = new NamespacedKey(this, "slime_rod");
+        
+        ShapelessRecipe recipe = new ShapelessRecipe(key, SlimeRod);
+        recipe.addIngredient(Material.END_ROD);
+        recipe.addIngredient(8,Material.SLIME_BALL);
+        
+        Bukkit.addRecipe(recipe);
     }
 }
--- src/main/java/org/WHITECN/rods/SlimeRod.java
+++ src/main/java/org/WHITECN/rods/SlimeRod.java
@@ -6,7 +6,10 @@ import org.WHITECN.anendrod;
 import org.bukkit.NamespacedKey;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Slime;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -21,23 +24,29 @@ import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
 
 import java.util.List;
+import java.util.Random;
 
-public class RegularRod implements Listener {
+public class SlimeRod implements Listener {
+    private Random random = new Random();
     @EventHandler
     public void onRegularRod(PlayerInteractEvent event) {
         Player player = event.getPlayer();
         ItemStack mainHand = event.getItem();
         if (mainHand != null && (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)) {
             ItemMeta meta = mainHand.getItemMeta();
-            if (meta != null && meta.getDisplayName().equals(rodItemGenerator.createRegularRod().getItemMeta().getDisplayName())) {
+            if (meta != null && meta.getDisplayName().equals(rodItemGenerator.createSlimeRod().getItemMeta().getDisplayName())) {
                 event.setCancelled(true);
                 mainHand.setItemMeta(useCounter.addTime(meta));
-                meta.setLore(List.of("§7没什么特别的 就是末地烛哦\n","§7已使用 §e" + meta.getPersistentDataContainer().get(new NamespacedKey(anendrod.getInstance(),"useCount"), PersistentDataType.INTEGER) + "§7 次"));
+                meta.setLore(List.of("§7一个黏糊糊的末地烛哦\n","§7已使用 §e" + meta.getPersistentDataContainer().get(new NamespacedKey(anendrod.getInstance(),"useCount"), PersistentDataType.INTEGER) + "§7 次"));
                 mainHand.setItemMeta(meta);
                 player.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS,40,0));
                 player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION,100,4));
-                player.damage(1.0d);
+                player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW,5*20,4));
+                player.damage(1d);
                 player.setVelocity(new Vector(0, 0.25, 0));
+                for (int i = 0; i < Math.abs(random.nextInt(5)) + 1; i++) {
+                    Slime entity = (Slime) player.getWorld().spawnEntity(player.getLocation(), EntityType.SLIME); entity.setSize(1);
+                }
                 player.setNoDamageTicks(0);
                 player.playSound(player, Sound.BLOCK_HONEY_BLOCK_SLIDE, 100.0f, 1.0f);
                 player.spawnParticle(Particle.HEART,player.getLocation(),30,1.5d,1.0d,1.5d);
